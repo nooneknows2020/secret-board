d7fba472a0098b2dde0f40776c0d8e7771cf12a7
nooneknows2020 <e_dends@yahoo.co.jp>
2020-07-17 16:22:36 +0900

    認証機能、ログアウト、404対応、投稿内容一覧ページの追加

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict';
 const http = require('http');
+const auth = require('http-auth');
 const router = require('./lib/router');
 
+// Basic認証
+const basic = auth.basic({
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
 // サーバの実装
-const server = http.createServer((req, res) => {
+const server = http.createServer(basic, (req, res) => {
   // routerモジュールのroute関数を呼び出す
   router.route(req, res);
 }).on('error', (e) => {
